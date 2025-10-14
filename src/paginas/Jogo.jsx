c4c2b392ab73ef4339cfc7e2981127fc2661d9bc
Raphael Martins <laraqipe@gmail.com>
2025-10-13 21:11:29 -0300

    first commit

---
+++ src/paginas/Jogo.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+export default function Jogo() {
+  return (
+    <div>
+      <h1>Home Page 🏡</h1>
+      <Link to="/jogo">Ir para About</Link>
+    </div>
+  );
+}
+
