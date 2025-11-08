316fc6d07b692cc9f1017f323cd9030a785d5615
Beatriz Claudino <beatrizclaudino09@gmail.com>
2025-11-08 00:33:31 -0300

    imagens site educativo e layout completo

--- src/paginas/EducaCidade.jsx
+++ src/paginas/EducaCidade.jsx
@@ -1,8 +1,9 @@
 import img from "../img/alerta_celular.png"
 import img1 from "../img/coferir_chuva.png"
 import img2 from "../img/pegar_doc.png"
-import img3 from "../img/local_seguro.jpeg"
-import img4 from "../img/nubi_oculos.png"
+import img3 from "../img/local_seguro.png"
+import img4 from "../img/nubi_professor.png"
+import img5 from "../img/familia_reunida.png"
 import { useEffect, useState } from "react"
 
 export default function EducaCidade() {
@@ -31,6 +32,56 @@ export default function EducaCidade() {
         "Mantenha a calma e siga as orientações oficiais."
     ];
 
+    const alertasInundacao = [
+        {
+            titulo: "Leia atentamente a mensagem de alerta gerada pelo Risk Mapping.",
+            imagemSrc: img,
+            dicas: [
+                "Mantenha a calma. A informação é poder!",
+                'O alerta de **"HIGH FLOOD RISK"** (Alto Risco de Inundação) indica que sua área pode ser afetada em breve.',
+                "Confirme sua localização e verifique no mapa do Risk Mapping os **abrigos e rotas de fuga seguros** mais próximos."
+            ]
+        },
+        {
+            titulo: "Não espere a água subir!",
+            imagemSrc: img1,
+            dicas: [
+                "Observe o nível da água na rua. O sinal de perigo indica que o risco é real e imediato.",
+                "Não se arrisque atravessando áreas já alagadas. A água pode esconder buracos, objetos perigosos e a força da correnteza pode derrubar uma pessoa.",
+                "Desligue a **energia elétrica** (o disjuntor principal) da sua casa se a água começar a invadir o imóvel. Isso evita curtos-circuitos e choques elétricos fatais."
+            ]
+        },
+        {
+            titulo: "Prepare-se para Deixar o Local",
+            imagemSrc: img2,
+            dicas: [
+                "Pegue o seu **'Kit de Emergência'**: Este kit deve estar em um local alto e de fácil acesso (como a parte superior de um armário).",
+                "**Documentos são prioridade**: Guarde em um saco plástico à prova d'água seus documentos pessoais (identidade, certidões, carteira de vacinação, etc.).",
+                "**Itens Essenciais**: Inclua água potável, alimentos não perecíveis (enlatados), lanterna, rádio à pilha e medicamentos de uso contínuo."
+            ]
+        },
+        {
+            titulo: "Busque Abrigo em um Local Alto e Seguro",
+            imagemSrc: img3,
+            dicas: [
+                "Use o Risk Mapping para seguir as rotas seguras até os pontos de abrigo indicados.",
+                "Caminhe em áreas mais altas e evite margens de rios, canais e encostas(risco de deslizamento).",
+                "Comunique - se: Se for possível, avise familiares ou vizinhos para onde você está indo.",
+                "Lembre - se: A sua vida e a de sua família são o bem mais importante.Os bens materiais podem ser recuperados, a vida não!"
+            ]
+        },
+        {
+            titulo: "Juntos Somos Mais Fortes!",
+            imagemSrc: img5,
+            dicas: [
+                "Ajude sua comunidade: Ofereça apoio a vizinhos, especialmente idosos, crianças e pessoas com mobilidade reduzida, que podem precisar de ajuda extra para se preparar ou se deslocar.",
+                "Mantenha contato: Troque informações com seus vizinhos sobre a situação e as rotas de segurança.",
+                "Prepare - se em grupo: Discutam em família e com a comunidade um plano de emergência, definindo pontos de encontro e formas de comunicação.",
+                "O Nubi está aqui para lembrar: Em momentos de desafio, a solidariedade e a colaboração fazem toda a diferença.Cuidar uns dos outros é a melhor forma de proteger a todos!"
+            ]
+        }
+    ];
+
     const [frase, setFrase] = useState("");
 
     useEffect(() => {
@@ -41,27 +92,24 @@ export default function EducaCidade() {
 
     return (
         <div className="flex items-center flex-col bg-center w-full dark:bg-[#1e293b]">
-            <div className="w-full h-[250px] lg:h-full flex flex-col items-center justify-center bg-gradient-to-r from-[#2da0af] to-blue-500 dark:bg-[#22415A]">
-                <div className="flex w-[90%] md:w-[70%] flex-col items-center ">
-                    <div className="w-full flex lg:items-center justify-center space-x-6">
-                        <div className="hidden sm:flex flex-col text-left justify-center">
-                            <p className="text-2xl xl:text-3xl font-bold text-amber-50">Nubi conselhos</p>
+            <div className="w-full h-[250px] lg:h-[300px] flex flex-col items-center justify-center bg-gradient-to-r from-[#2da0af] to-blue-500 dark:bg-[#22415A]">
+                <div className="flex w-[90%] sm:w-[70%] md:w-[60%] xl:w-[40%] lg:justify-center flex-col items-center lg:gap-y-2">
+                    <div className="w-full md:w-full flex items-center">
+                        <div className="flex w-full flex-col text-left justify-center text-gray-100 gap-y-3">
+                            <p className="text-2xl xl:text-5xl md:text-3xl font-bold">Nubi conselhos</p>
+                            <p className="hidden lg:flex text-sm lg:text-lg">Além de dar conselhos, o Nubi mostra como se proteger e quais passos seguir durante um alagamento. Segurança vem em primeiro lugar!</p>
                         </div>
-
-                        <div className="flex flex-col sm:hidden text-left justify-center">
-                            <p className="text-2xl xl:text-3xl font-bold text-amber-50">Nubi</p>
-                            <p className="text-2xl xl:text-3xl font-bold text-amber-50">conselhos </p>
-                        </div>
-
-                        <img
-                            className={`h-[100px] w-[140px] xl:h-[250px] xl:w-[360px] transform transition-all duration-1000 
+                        <div className="w-1/2 ">
+                            <img
+                                className={`w-full h-full transform transition-all duration-1000 
                           ${animateNubi ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-20"}`}
-                            src={img4}
-                            alt="Nubi"
-                        />
+                                src={img4}
+                                alt="Nubi"
+                            />
+                        </div>
                     </div>
-                    <div className="w-full bg-[#1a3246] border border-cyan-300 p-5 rounded-2xl text-amber-50 font-semibold break-words text-left lg:w-[50%] ml-4">
-                        <p className="text-lg xl:text-lg">{frase}</p>
+                    <div className="w-full bg-[#1a3246] border border-cyan-300 p-5 rounded-2xl text-gray-100 font-semibold break-words text-left ml-4">
+                        <p className="xl:text-lg">Dica: {frase}</p>
                     </div>
                 </div>
             </div>
@@ -77,7 +125,7 @@ export default function EducaCidade() {
                         <stop offset="100%" stopColor="#3b82f6" /> {/* to-blue-500 */}
                     </linearGradient>
 
-                   <linearGradient id="lightGradient" x1="0%" y1="0%" x2="100%" y2="0%">
+                    <linearGradient id="lightGradient" x1="0%" y1="0%" x2="100%" y2="0%">
                         <stop offset="0%" stopColor="#2da0af" /> {/* from-green-400 */}
                         <stop offset="100%" stopColor="#3b82f6" /> {/* to-blue-500 */}
                     </linearGradient>
@@ -92,8 +140,8 @@ export default function EducaCidade() {
             </svg>
 
 
-            <div className="w-[90%] flex items-center flex-col gap-y-8 mt-8">
-                <div className="w-full h-[400px] flex items-center justify-center max-w-3xl aspect-video rounded-xl overflow-hidden shadow-lg">
+            <div className="w-[90%] sm:w-[80%] md:w-[70%] lg:w-[80%] xl:w-[60%] sm:p-5 shadow-lg flex items-center flex-col gap-y-6 lg:gap-y-16 mt-6">
+                <div className="w-full h-[300px] lg:h-[500px]  aspect-video rounded-xl overflow-hidden shadow-lg">
                     <iframe
                         width="100%"
                         height="100%"
@@ -104,17 +152,26 @@ export default function EducaCidade() {
                         allowFullScreen
                     ></iframe>
                 </div>
-                <div>
-                    <img className="h-[250px] lg:h-[400px] w-[600px]" src={img} alt="" />
-                </div>
-                <div>
-                    <img className="h-[250px] lg:h-[400px] w-[600px]" src={img1} alt="" />
-                </div>
-                <div>
-                    <img className="h-[250px] lg:h-[400px] w-[600px]" src={img2} alt="" />
-                </div>
-                <div>
-                    <img className="h-[250px] lg:h-[400px] w-[600px]" src={img3} alt="" />
+                <div className=" h-full grid gap-y-6 lg:gap-y-16 rounded-lg hover:shadow-xl transition-all bg-white dark:bg-[#1b263b] dark:text-white">
+                    {alertasInundacao.map((alerta, index) => (
+                        <div className="lg:flex lg:justify-center lg:items-center rounded-2xl dark:border border-[#2da0af] grid gap-y-4 gap-x-8 shadow-2xl p-4 lg:p-6" key={index}>
+                            <div className="grid xl:gap-y-4">
+                                <span className="text-lg xl:text-xl dark:text-gray-100 text-gray-900 font-medium">{alerta.titulo}</span>
+                                <img
+                                    className="h-[230px] sm:h-[300px] lg:h-[400px] w-[600px]"
+                                    src={alerta.imagemSrc}
+                                    alt={`Imagem de alerta sobre ${alerta.titulo}`}
+                                />
+                            </div>
+                            <div className="lg:w-1/3">
+                                <ul className="list-disc pl-6 grid gap-y-4">
+                                    {alerta.dicas.map((dica, dicaIndex) => (
+                                        <li className="bg-[#5e90bb11] dark:bg-[#31506b59] dark:text-gray-100 p-2 xl:p-4 rounded-md text-gray-900" key={dicaIndex} dangerouslySetInnerHTML={{ __html: dica }}></li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div >
